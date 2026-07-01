02dfc501edaba1d5654eb0d84dae7cfd58245a0e
ksm30289 <ksm30289@gmail.com>
2026-07-01 12:17:59 +0900

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,18 +1,37 @@
 import gspread
-
 from google.oauth2.service_account import Credentials
 
-from config import GOOGLE_CREDENTIALS
+from config import (
+    GOOGLE_CREDENTIALS,
+    SPREADSHEET_ID,
+    SHEET_NAME,
+)
 
 
 SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets"
+    "https://www.googleapis.com/auth/spreadsheets",
 ]
 
 
-credentials = Credentials.from_service_account_info(
-    GOOGLE_CREDENTIALS,
-    scopes=SCOPES
-)
+def get_worksheet():
+    credentials = Credentials.from_service_account_info(
+        GOOGLE_CREDENTIALS,
+        scopes=SCOPES,
+    )
 
-gc = gspread.authorize(credentials)
+    client = gspread.authorize(credentials)
+
+    spreadsheet = client.open_by_key(SPREADSHEET_ID)
+
+    return spreadsheet.worksheet(SHEET_NAME)
+
+
+def append_market_row(row):
+    worksheet = get_worksheet()
+
+    worksheet.append_row(
+        row,
+        value_input_option="USER_ENTERED",
+    )
+
+    print("[Sheets] 저장 완료")
