c6bc188065b0dc4ad4f03bac8c9010edc98909e2
ksm30289 <ksm30289@gmail.com>
2026-07-03 11:45:08 +0900

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,5 +1,4 @@
 import gspread
-
 from google.oauth2.service_account import Credentials
 
 from config import (
@@ -8,13 +7,18 @@ from config import (
     SHEET_NAME,
 )
 
-
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
 ]
 
+_cached_worksheet = None
+
 
 def get_worksheet():
+    global _cached_worksheet
+
+    if _cached_worksheet:
+        return _cached_worksheet
 
     credentials = Credentials.from_service_account_info(
         SERVICE_ACCOUNT_INFO,
@@ -27,17 +31,17 @@ def get_worksheet():
         SPREADSHEET_ID
     )
 
-    return spreadsheet.worksheet(
+    _cached_worksheet = spreadsheet.worksheet(
         SHEET_NAME
     )
 
+    return _cached_worksheet
+
 
 def find_date_row(worksheet, date_text):
-
     values = worksheet.col_values(1)
 
     for index, value in enumerate(values, start=1):
-
         if value == date_text:
             return index
 
@@ -45,27 +49,6 @@ def find_date_row(worksheet, date_text):
 
 
 def save_market(row):
-
-    """
-    row
-
-    A 날짜
-    B 수집시간
-
-    C 아이템매니아 평균
-    D 아이템매니아 최고
-    E 아이템매니아 최저
-    F 아이템매니아 매물수
-
-    G 오픈톡 평균
-    H 오픈톡 최저
-    I 오픈톡 최고
-    J 거래건수
-
-    K 비고
-    L 특이사항
-    """
-
     worksheet = get_worksheet()
 
     date_text = row[0]
@@ -76,37 +59,32 @@ def save_market(row):
     )
 
     if target_row:
+        # B 수집시간만 수정
+        worksheet.update_acell(
+            f"B{target_row}",
+            row[1],
+        )
 
-        # B~L만 수정
+        # G~L 오픈톡 영역만 수정
         worksheet.update(
-            f"B{target_row}:L{target_row}",
+            f"G{target_row}:L{target_row}",
             [[
-                row[1],   # B
-                row[2],   # C
-                row[3],   # D
-                row[4],   # E
-                row[5],   # F
-                row[6],   # G
-                row[7],   # H
-                row[8],   # I
-                row[9],   # J
-                row[10],  # K
-                row[11],  # L
+                row[6],   # G 오픈톡 평균
+                row[7],   # H 오픈톡 최저
+                row[8],   # I 오픈톡 최고
+                row[9],   # J 오픈톡 거래건수
+                row[10],  # K 비고
+                row[11],  # L 특이사항
             ]],
             value_input_option="USER_ENTERED",
         )
 
-        print(
-            f"[Sheets] {date_text} 업데이트 완료"
-        )
+        print(f"[Sheets] {date_text} 업데이트 완료")
 
     else:
-
         worksheet.append_row(
             row,
             value_input_option="USER_ENTERED",
         )
 
-        print(
-            f"[Sheets] {date_text} 추가 완료"
-        )
+        print(f"[Sheets] {date_text} 추가 완료")
